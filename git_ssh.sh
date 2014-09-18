3dea2c72d183d01e5f12a5f5bd3f1f47b131d4d9
leaven <dlwelllee@gmail.com>
2014-09-19 02:02:17 +0800

    change

--- git_ssh.sh
+++ git_ssh.sh
@@ -2,6 +2,8 @@
 createRSA ( ) {
 	read -p "enter your_email@example.com : " email
 	ssh-keygen -t rsa -C  $email
+	eval "$(ssh-agent -s)"
+	ssh-add ~/.ssh/id_rsa
 }
 
 number=$(ls -lR ~/.ssh|grep "id_*"|wc -l)
